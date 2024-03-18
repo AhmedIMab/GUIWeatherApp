ac1715dd421ac77c59db0a8046336a238681737d
Jas111111 <lzw878920787@qq.com>
2024-03-18 22:19:45 +0800

    fix hourly weather

--- src/components/ElementPartlyCloudy/ElementPartlyCloudy.jsx
+++ src/components/ElementPartlyCloudy/ElementPartlyCloudy.jsx
@@ -7,11 +7,11 @@ import PropTypes from "prop-types";
 import React from "react";
 import "./style.css";
 
-export const ElementPartlyCloudy = ({ color, className, outline = "/img/outline-32.svg" }) => {
+export const ElementPartlyCloudy = ({ color, className, outline = "09d" }) => {
   return (
     <div className={`element-partly-cloudy ${className}`}>
-      <img className="outline" alt="Outline" src={color === "on" ? outline : "/img/outline-33.svg"} />
-      {/* <img className="outline"  src={`http://openweathermap.org/img/w/${currentWeatherData.icon}.png`} alt="weather icon" /> */}
+      {/* <img className="outline" alt="Outline" src={color === "on" ? outline : "/img/outline-33.svg"} /> */}
+      <img className="outline"  src={`http://openweathermap.org/img/w/${outline}.png`} alt="weather icon" />
     </div>
   );
 };
--- src/components/WeatherTotal/WeatherTotal.jsx
+++ src/components/WeatherTotal/WeatherTotal.jsx
@@ -24,14 +24,14 @@ export function WeatherTotal() {
   const [forecastData, setForecastData] = useState(null);
 
   // hourly data weather icon
-  const[lowest_temperature, set_lowest_temperature] = useState(["22", "25", "26", "23", "24",  "27", "28"]);
-  const[highest_temperature, set_highest_temperature] = useState(["25", "28", "29", "26", "27",  "30", "31"]);
+  const [lowest_temperature, set_lowest_temperature] = useState(["22", "25", "26", "23", "24", "27", "28"]);
+  const [highest_temperature, set_highest_temperature] = useState(["25", "28", "29", "26", "27", "30", "31"]);
 
   // according to the selectedDay, call weather API to change the following data
   // 1.prop
   const [prop, setProp] = useState([17, 12, 21, 15, 7, 9, 9]);
   // 2.weather icon
-  const [weatherIcon, setWeatherIcon] = useState(["/outline-17.svg", "/outline-15.svg", "/outline-13.svg", "/outline-11.svg", "/union-4.svg", "/outline-9.svg", "/outline-7.svg"]);
+  const [weatherIcon, setWeatherIcon] = useState(["09d", "09d", "09d", "09d", "09d", "09d", "09d"]);
   const [weatherIconDaily, setWeatherIconDaily] = useState(["09d", "09d", "09d", "09d", "09d", "09d", "09d"]);
 
   // 3.temperature svg
@@ -42,6 +42,8 @@ export function WeatherTotal() {
 
   // forecastDataObject
   const [forecastDataObject, setForecastDataObject] = useState(null);
+  
+  const [currentHour, setCurrentHour] = useState(new Date().getHours());
 
   const fetchCurData = async () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a484704a1f7fd5d6f7fa69419cdbf252`) // replace {YOUR_API_KEY} with your OpenWeatherMap API key
@@ -62,63 +64,69 @@ export function WeatherTotal() {
       });
   }
 
+  // function for changing the weather code to the icon
+  const changeWeatherCodeToIcon = (weatherCode) => {
+    for (let i = 0; i < weatherCode.length; i++) {
+      if (weatherCode[i] === 0) {
+        weatherCode[i] = '01d';
+      }
+      else if (weatherCode[i] === 1) {
+        weatherCode[i] = '02d';
+      }
+      else if (weatherCode[i] === 2) {
+        weatherCode[i] = '03d';
+      }
+      else if (weatherCode[i] === 3) {
+        weatherCode[i] = '04d';
+      }
+      else if (weatherCode[i] === 45 || weatherCode[i] === 48) {
+        weatherCode[i] = '50d';
+      }
+      else if (50 < weatherCode[i] < 66) {
+        weatherCode[i] = '09d';
+      }
+      else if (70 < weatherCode[i] < 80) {
+        weatherCode[i] = '13d';
+      }
+      else if (79 < weatherCode[i] < 90) {
+        weatherCode[i] = '10d';
+      }
+      else if (90 < weatherCode[i] < 100) {
+        weatherCode[i] = '11d';
+      }
+    }
+    return weatherCode;
+  };
+
   // fetch data for the next 5 days
   const fetchData = async () => {
     try {
       // 7 day weather forecast (hourly temperature and precipitation probability) and current is_day
-      const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=is_day&hourly=temperature_2m,precipitation_probability&&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe/London`);
-      const weatherCode = weatherData.data.daily.weather_code;
-
-      // set weather icon for daily weather
-      for (let i = 0; i < weatherData.data.daily.weather_code.length; i++) {
-          if (weatherCode[i] === 0) {
-              weatherCode[i] = '01d';
-          }
-          else if (weatherCode[i] === 1) {
-              weatherCode[i] = '02d';
-          }
-          else if (weatherCode[i] === 2) {
-              weatherCode[i] = '03d';
-          }
-          else if (weatherCode[i] === 3) {
-              weatherCode[i] = '04d';
-          }
-          else if (weatherCode[i] === 45 || weatherCode[i] === 48) {
-              weatherCode[i] = '50d';
-          }
-          else if (50 < weatherCode[i] < 66) {
-              weatherCode[i] = '09d';
-          }
-          else if (70 < weatherCode[i] < 80) {
-              weatherCode[i] = '13d';
-          }
-          else if (79 < weatherCode[i] < 90) {
-              weatherCode[i] = '10d';
-          }
-          else if (90 < weatherCode[i] < 100) {
-              weatherCode[i] = '11d';
-          }
-      }
-
+      const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=is_day&hourly=temperature_2m,precipitation_probability,weather_code&&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe/London`);
+      const dailyWeatherCode = changeWeatherCodeToIcon(weatherData.data.daily.weather_code);
+      const hourlyWeatherCode = changeWeatherCodeToIcon(weatherData.data.hourly.weather_code);
+      
       const forecastDataObjectTmp = {
         // daily
         dailyData: {
           maxTemp: weatherData.data.daily.temperature_2m_max,
           minTemp: weatherData.data.daily.temperature_2m_min,
           date: weatherData.data.daily.time,
-          icon: weatherCode
+          icon: dailyWeatherCode
         },
         hourlyData: {
           precipitationProbability: weatherData.data.hourly.precipitation_probability,
           temperature: weatherData.data.hourly.temperature_2m,
-          date: weatherData.data.hourly.time
+          date: weatherData.data.hourly.time,
+          icon: hourlyWeatherCode
         }
       };
       setForecastDataObject(forecastDataObjectTmp);
       set_lowest_temperature(forecastDataObjectTmp.dailyData.minTemp);
       set_highest_temperature(forecastDataObjectTmp.dailyData.maxTemp);
       setWeatherIconDaily(forecastDataObjectTmp.dailyData.icon);
-
+      setWeatherIcon(forecastDataObjectTmp.hourlyData.icon);
+      // console.log(forecastDataObjectTmp.hourlyData.icon)
       // add is_day to the current weather data object
       const currentWeatherDataObject = {
         ...currentWeatherData,
@@ -161,15 +169,15 @@ export function WeatherTotal() {
 
           {/* weather icon */}
           <div className="dock-section">
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[0]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[1]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[2]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[3]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[4]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[5]} />
-            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[6]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[selectedDay * 24 + currentHour]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 1) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 2) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 3) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 4) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 5) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
+            <ElementPartlyCloudy className="dock-img" color="on" outline={weatherIcon[(selectedDay * 24 + currentHour + 6) >= 167 ? (selectedDay * 24 + currentHour + 1) : 167]} />
           </div>
-          
+
           {/* rainfall graph --- TODO */}
           <div className="dock-section">
             <img className="graph" alt="Vector" src={temperature} />
@@ -189,23 +197,23 @@ export function WeatherTotal() {
           {/* Rainfall probability */}
           <div className="dock-section">
             <div className="temp-text">{prop[0]}%</div>
-            <div className="temp-text">{prop[1]}%</div>
-            <div className="temp-text">{prop[2]}%</div>
-            <div className="temp-text">{prop[3]}%</div>
-            <div className="temp-text">{prop[4]}%</div>
-            <div className="temp-text">{prop[5]}%</div>
-            <div className="temp-text">{prop[6]}%</div>
+            <div className="temp-text">{prop[1] == null ? prop[0] : prop[1]}%</div>
+            <div className="temp-text">{prop[2] == null ? prop[0] : prop[1]}%</div>
+            <div className="temp-text">{prop[3] == null ? prop[0] : prop[1]}%</div>
+            <div className="temp-text">{prop[4] == null ? prop[0] : prop[1]}%</div>
+            <div className="temp-text">{prop[5] == null ? prop[0] : prop[1]}%</div>
+            <div className="temp-text">{prop[6] == null ? prop[0] : prop[1]}%</div>
           </div>
 
         </div>
       </div>
 
       {/* Daily weather ------ section 4*/}
-      <DailyWeather setSelectedDay={updateDay} 
-                    selectedDay={selectedDay} 
-                    lowest_temperature={lowest_temperature}
-                    highest_temperature={highest_temperature}
-                    weatherIconDaily={weatherIconDaily}/>
+      <DailyWeather setSelectedDay={updateDay}
+        selectedDay={selectedDay}
+        lowest_temperature={lowest_temperature}
+        highest_temperature={highest_temperature}
+        weatherIconDaily={weatherIconDaily} />
     </>
 
   );
